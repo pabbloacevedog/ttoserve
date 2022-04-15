318fb7aa1ed5942eafe3ef0302d8eef7171bac6a
paceved.11 <pacevedo@aligare.cl>
2022-04-14 21:46:46 -0400

    graphql

--- src/setup/graphql.js
+++ src/setup/graphql.js
@@ -1,32 +1,35 @@
 // Imports
-const { ApolloServer } = require('apollo-server-express');
+import graphqlHTTP from "express-graphql";
+const { ApolloServer } = require("apollo-server-express");
 // App Imports
-import schema from '../schema'
+import config from "../config/config.json";
+import schema from "../schema";
 
 // Setup GraphQL
 export default function (server) {
-  console.info('SETUP - GraphQL...')
-  const graphQLServer = new ApolloServer({
-	schema,
-  });
-  graphQLServer.applyMiddleware({
-	app: server,
-	cors: {
-	  origin: true,
-	  credentials: true,
-	  methods: ['POST'],
-	  allowedHeaders: [
-		'X-Requested-With',
-		'X-HTTP-Method-Override',
-		'Content-Type',
-		'Accept',
-		'Authorization',
-		'Access-Control-Allow-Origin',
-	  ],
-	},
-	playground: {
-	  settings: {
-		'editor.theme': 'light',
-	  },
-	},
-  });
+	console.info("SETUP - GraphQL...");
+	const graphQLServer = new ApolloServer({
+		schema,
+	});
+	graphQLServer.applyMiddleware({
+		app: server,
+		cors: {
+			origin: true,
+			credentials: true,
+			methods: ["POST"],
+			allowedHeaders: [
+				"X-Requested-With",
+				"X-HTTP-Method-Override",
+				"Content-Type",
+				"Accept",
+				"Authorization",
+				"Access-Control-Allow-Origin",
+			],
+		},
+		playground: {
+			settings: {
+				"editor.theme": "light",
+			},
+		},
+	});
+}
